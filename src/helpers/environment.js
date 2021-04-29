a8f8ea0934715e4622cefbdfbde112ee4588a3c1
Mia Casas <miacasas@mias-mbp.attlocal.net>
2021-04-29 11:28:36 -0500

    url

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -4,7 +4,7 @@ switch(window.location.hostname){
     case 'localhost' || '127.0.0.1':
         APIURL = 'http://localhost:5005';
         break;
-    case 'lista-list.herokuapp.com':
+    case 'lista-shopping-client.herokuapp.com':
         APIURL = 'https://lista-shopping-list.herokuapp.com'
 }
 
