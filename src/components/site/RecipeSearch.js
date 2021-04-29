e5ef8cfa9b69ac7d7046e15dd016987e456213b3
William Lusk <79818401+wmlusk@users.noreply.github.com>
2021-04-28 21:48:02 -0400

    2021Apr28-21:47

--- src/components/site/RecipeSearch.js
+++ src/components/site/RecipeSearch.js
@@ -1,17 +1,16 @@
 import React, { useState } from "react";
-import { Col, Row } from "reactstrap";
+import { Col, Row, Button } from "reactstrap";
+import APIURL from '../../helpers/environment'
+// import RecipeSearchResults from "./RecipieSearchResults";
 require("dotenv").config();
 
-const RecipeSearch = () => {
+const RecipeSearch = (props) => {
   const [recipe1, displayRecipes1] = useState([]);
   const [recipe2, displayRecipes2] = useState([]);
   const [recipe3, displayRecipes3] = useState([]);
   const [title1, displayTitle1] = useState([]);
   const [title2, displayTitle2] = useState([]);
   const [title3, displayTitle3] = useState([]);
-
-  //RECIPE IMAGES????
-
   const [search, setSearch] = useState({
     food: "",
     diet: "",
@@ -37,29 +36,50 @@ const onSubmitForm = (event) => {
   // Create Search URL variable
   let url = `${baseURL}search?q=${food.value}&health=${diet.value}&cuisineType=${cuisineType.value}&mealType=${mealType.value}&app_id=${appId}&app_key=${appKey}`;
 
-  // Insert Mean Girls Joke about making fetch happen
+  // Fetch
       fetch(url)
         .then((res) => res.json())
         .then((props) => {
           // Recipe 1
-          displayRecipes1(props.hits[8].recipe.ingredientLines);
-          displayTitle1(props.hits[8].recipe.label);
+          displayRecipes1(props.hits[1].recipe.ingredientLines);
+          displayTitle1(props.hits[1].recipe.label);
           // Recipe 2
-          displayRecipes2(props.hits[9].recipe.ingredientLines);
-          displayTitle2(props.hits[9].recipe.label);
+          displayRecipes2(props.hits[2].recipe.ingredientLines);
+          displayTitle2(props.hits[2].recipe.label);
           // Recipe 3
-          displayRecipes3(props.hits[4].recipe.ingredientLines);
-          displayTitle3(props.hits[4].recipe.label);
+          displayRecipes3(props.hits[3].recipe.ingredientLines);
+          displayTitle3(props.hits[3].recipe.label);
         });
     };
 
-  //Convert recipe arrays into list items
-
+//Convert recipe arrays into list items
   const recipeList1 = recipe1.map((recipe1) => <li>{recipe1}</li>);
   const recipeList2 = recipe2.map((recipe2) => <li>{recipe2}</li>);
   const recipeList3 = recipe3.map((recipe3) => <li>{recipe3}</li>);
 
-  //Return
+
+// Using array.join to remove commas
+// items in array still have special characters which cause an error in the console log
+
+  let recipe1out = recipe1.join();
+  let recipe2out = recipe2.join();
+  let recipe3out = recipe3.join();
+
+// Save Search Results 
+/* Need to figure out a function or if/then scenario to 
+allow whichever recipes is slectect to populate the title/ingredients
+*/
+
+const saveSearch = (e) => {
+  e.preventDefault();
+  fetch(`${APIURL}/search/create`, {
+    method: "POST",
+    body: JSON.stringify({search: {label:title2, ingredients:recipe2out}}),
+    headers: new Headers ({'Content-Type': 'application/json','Authorization': `Bearer ${props.token}`})
+    })
+  } // There is no notification of success or failure happening
+
+ //Return
   return (
     <div className="main">
       <div className="mainDiv">
@@ -67,23 +87,18 @@ const onSubmitForm = (event) => {
          {/* SEARCH FORM */}
         <Col>
         <form onSubmit={(e) => onSubmitForm(e)} >
-     
-          <label for="food">Main Food:</label>
+         <label for="food">Main Food:</label>
           <input
             type="text" name="food"
           />
-  
           <label for="diet">Diet Preference:</label>
           <input
             type="text" name="diet"
           />
-       
-      
-          <label for="cuisineType">Cuisine Type:</label>
+           <label for="cuisineType">Cuisine Type:</label>
           <input
             type="text" name="cuisineType"
             />
-
           <label for="mealType">Meal Type:</label>
           <input
             type="text"
@@ -94,20 +109,26 @@ const onSubmitForm = (event) => {
              </form>
         </Col>
       </div>
+      {/* Search Results */}
+      <div>
       <Row>
-        <Col>
-          {title1}
-          <ul>{recipeList1}</ul>
-        </Col>
-        <Col>
-          {title2}
-          <ul>{recipeList2}</ul>
-        </Col>
-        <Col>
-          {title3}
-          <ul>{recipeList3}</ul>
-        </Col>
-      </Row>
+    <Col>
+      {title1}
+      <ul>{recipeList1}</ul>
+      <ul>{recipeList1.length > 0 ? <Button type="submit" onClick={saveSearch}>Select</Button> : null}</ul>
+    </Col>
+    <Col>
+      {title2}
+      <ul>{recipeList2}</ul>
+      <ul>{recipeList2.length > 0 ? <Button type="submit" onClick={saveSearch} >Select</Button> : null}</ul>
+    </Col>
+    <Col>
+      {title3}
+      <ul>{recipeList3}</ul>
+      <ul>{recipeList3.length > 0 ? <Button type="submit" onClick={saveSearch}>Select</Button> : null}</ul>
+    </Col>
+  </Row>
+      </div>
     </div>
   );
 };
